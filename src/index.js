437b065fd0bc0a504132d3275bac33e95ca3e66a
Command2017 <daniel.steinegger@students.fhnw.ch>
2019-08-18 20:00:42 +0200

    add standarts and firstore change listener

--- src/index.js
+++ src/index.js
@@ -115,3 +115,177 @@ export const changeDocId = async function(docRef, newKey) {
     return Promise.reject(e)
   }
 }
+
+/***************************************************************************************/
+/** This class contains the standard C(R)UD functions for a database connection. It can be used as a object or in a static context */
+/** WARNING: Do this at your own risk, only do this if you are sure what you are doing */
+/***************************************************************************************/
+
+export class standards {
+  constructor(ref, name = '', asObject = false) {
+    this.ref = ref
+    this.name = name
+    this.asObject = asObject
+  }
+
+  async getAll() {
+    this.constructor.getAll(this.ref, this.asObject)
+  }
+  async get(id) {
+    this.constructor.get(id, this.ref)
+  }
+  async add(data) {
+    this.constructor.add(data, this.ref)
+  }
+  async set(data, id) {
+    this.set(data, id, this.ref)
+  }
+
+  async delete(id) {
+    this.constructor.delete(id, this.ref)
+  }
+
+  static async getAll(ref, asObject = false) {
+    try {
+      return await queryFirestore(ref, asObject)
+    } catch (e) {
+      console.error('DB error getAll() of ' + this.name + '\n' + e)
+    }
+  }
+  static async get(id, ref) {
+    try {
+      return await queryFirestore(ref.doc(id))
+    } catch (e) {
+      console.error('DB error get() of ' + this.name + '\n' + e)
+    }
+  }
+  static async add(data, ref) {
+    try {
+      return await addToFirestore(ref, data)
+    } catch (e) {
+      console.error('DB error add() in ' + this.name + '\n' + e)
+    }
+  }
+  static async set(data, id, ref) {
+    try {
+      return await setToFirestore(ref.doc(id), data)
+    } catch (e) {
+      console.error('DB error set() in ' + this.name + '\n' + e)
+    }
+  }
+
+  static async delete(id, ref) {
+    try {
+      await ref.doc(id).delete()
+    } catch (e) {
+      console.error('DB error delete() in ' + this.name + '\n' + e)
+    }
+  }
+}
+
+/***************************************************************************************/
+/** This class simplifies the workflow to set an on change listener of a firestore  */
+/** WARNING: Do this at your own risk, only do this if you are sure what you are doing */
+/***************************************************************************************/
+
+// example
+// const commitFunctionsForStore = {
+//   add: function exampleFunctionAdd(obj,storeName) {
+//     store.commit(storeName + '/' + 'ADD', obj)
+//   },
+//   update: function exampleFunctionUpdate(obj,storeName) {
+//     store.commit(storeName + '/' + 'UPDATE', obj)
+//   },
+//   remove: function exampleFunctionRemove(obj,storeName) {
+//     store.commit(storeName + '/' + 'REMOVE', obj)
+//   },
+//   removeAll: function exampleFunctionRemove(storeName) {
+//     store.commit(storeName + '/' + 'REMOVE_ALL')
+//   }
+// }
+
+export class Listener {
+  constructor(ref, commitFunctions, options = {}) {
+    this.listeners = []
+    this.ref = ref
+    this.storeName = _saveGet(options.storeName, '')
+    this.commitFunctions = commitFunctions
+    this.log = _saveGet(options.log, false)
+    this.refFunction = _saveGet(options.refFunction, null)
+  }
+  async getListener(resetListener = false) {
+    if (this.listeners.length > 1 || resetListener) {
+      this.detachListeners
+      console.info(this.storeName + ': To much listeners all detached')
+    }
+
+    if (this.listeners.length === 0) {
+      let ref = this.ref
+      if (this.refFunction != null) {
+        ref = this.refFunction(this.ref, this.options)
+      }
+      const listener = this.constructor.getStaticListener(ref, resetListener)
+
+      if (listener != null) {
+        this.listeners.push(listener)
+      }
+    }
+  }
+
+  static async getListener(ref, commitFunctions, options) {
+    const storeName = _saveGet(options.storeName, '')
+    const log = _saveGet(options.log, false)
+
+    if (!commitFunctions) console.error('No commitFunctions defined')
+
+    commitFunctions['removeAll'](storeName)
+
+    try {
+      const listener = await ref.onSnapshot(function(snapshot) {
+        snapshot.docChanges().forEach(function(change) {
+          const obj = change.doc.data()
+          obj.id = change.doc.id
+          obj.path = change.doc.ref.path
+
+          if (change.type === 'added') {
+            commitFunctions['add'](obj, storeName)
+
+            if (log) console.info('New ' + storeName + ': ', obj)
+          }
+          if (change.type === 'modified') {
+            commitFunctions['update'](obj, storeName)
+            if (log) console.info('New ' + storeName + ': ', obj)
+          }
+          if (change.type === 'removed') {
+            commitFunctions['remove'](obj, storeName)
+            if (log) console.info('New ' + storeName + ': ', obj)
+          }
+        })
+      })
+      if (log) console.info('All ' + storeName + ' listener atached')
+      return listener
+    } catch (e) {
+      console.error(
+        'DB error getStaticListener(ref) in ' + storeName + '\n' + e
+      )
+      return null
+    }
+  }
+
+  detachListeners() {
+    this.listeners.forEach(item => {
+      this.constructor.detachListeners(item)
+    })
+    this.listeners = []
+
+    if (this.log) console.info('All ' + this.storeName + ' listener Detached')
+  }
+
+  static detachListeners(listener) {
+    listener()
+  }
+}
+
+function _saveGet(data, default_value) {
+  return data ? data : default_value
+}
